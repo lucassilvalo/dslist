aea9c258e5fc5c686f30980448c48330880e29a5
lucassilvalo <lucasjwlopes@outlook.com>
2025-01-21 16:13:30 -0300

    Project created

---
+++ src/main/java/com/projectdevsuperior/pdslist/PdslistApplication.java
@@ -0,0 +1,13 @@
+package com.projectdevsuperior.pdslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectdevsuperior/pdslist/PdslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectdevsuperior.pdslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
